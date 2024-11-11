d32992c8ca39599b54ea18b99d82278dd2387a9b
Oleksandra <alexa30che@gmail.com>
2024-11-11 11:41:40 +0200

    Add Class List

--- ForwardList/main.cpp
+++ ForwardList/main.cpp
@@ -1,26 +1,34 @@
 ﻿#include "ForwardList.h"
+#include "List.h"
 #include <iostream>
 using namespace std;
 int main()
 {
-    ForwardList list;
+    /*ForwardList list;
     list.push_back(1);
     list.push_back(2);
     list.push_back(3);
     list.push_front(4);
     list.print();
-    /*Node* first_node = list.GetHead()->next->next;
+    Node* first_node = list.GetHead()->next->next;
     list.push_after(first_node, 5);
     list.print();
     const Node* res = list.find(2);
     if (res)
     {
         cout << "Result: " << res->value;
-    }*/
+    }
 
     ForwardList list2(list);
     list2.print();
     ForwardList list3 = list2;
-    list3.print();
+    list3.print();*/
+
+    List list;
+    list.push_back(10);
+    list.push_back(1);
+    list.push_back(-5);
+    list.push_front(0);
+    list.show();
     return 0;
 }
